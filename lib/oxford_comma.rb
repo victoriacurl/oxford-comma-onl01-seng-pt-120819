eb7e6410a622ef7f1d6cf903b789d520f4a113b1
Victoria Curl <victoriacurl9@gmail.com>
2019-12-22 13:14:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,7 @@
 def oxford_comma(array)
   return array.first if array.size == 1
 
+<<<<<<< HEAD
 
   if array.size == 2
     array.insert(1, "and").join(" ")
@@ -9,3 +10,6 @@ def oxford_comma(array)
     array.join(", ")
   end
 end 	
+=======
+end
+>>>>>>> 7b57ea5294542db4d5bda0adaaef904854416db8
